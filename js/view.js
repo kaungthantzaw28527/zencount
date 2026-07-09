17d405053909c9991e4435def8d4ebd7c4d7308f
Kaung Thant Zaw <kaungthantzaw28527@gmail.com>
2026-07-10 04:57:40 +0630

    fix: change iframe to object tag to resolve mobile PDF blob preview and scrolling issues

--- js/view.js
+++ js/view.js
@@ -48,7 +48,7 @@ class ZenCountView {
             this.counterContainer.classList.replace('col-lg-12', 'col-lg-4');
             
             if (fileType === 'pdf') {
-                this.pdfViewer.src = `https://docs.google.com/gview?url=${encodeURIComponent(fileUrl)}&embedded=true`;
+                this.pdfViewer.data = fileUrl; 
                 this.pdfViewer.classList.remove('d-none');
                 this.imageViewer.classList.add('d-none');
             } else {
